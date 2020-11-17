40ec67d36992f53e848a6e4d9e5f7bec288fb90e
jhen852 <jhenny.callapa.quispe@gmail.com>
2020-11-17 16:48:48 -0600

    Version 3

--- Ejercicio 2/Program.cs
+++ Ejercicio 2/Program.cs
@@ -9,11 +9,17 @@ namespace Ejercicio_2
             Console.WriteLine("Vector");
             int n, suma;
             Vector V1;
+
+            Console.WriteLine("Tamaño del Vector ");
             n = int.Parse(Console.ReadLine());
             V1 = new Vector(n);
             V1.LlenarVector();
             Console.WriteLine("Mostrando el Vector");
             V1.mostrarVector();
+            V1.OrdenarVector();
+            V1.mostrarVector();
+            suma = V1.SumaDeElementos();
+            Console.WriteLine("\nLa suma es "+suma);
             Console.ReadKey();
         }
     }
--- Ejercicio 2/Vector.cs
+++ Ejercicio 2/Vector.cs
@@ -6,39 +6,62 @@ namespace Ejercicio_2
 {
     class Vector
     {
-        //Atributos del vector 
-        //para declarar un vector tenemos que poner los corchetes delante de la variable del vector
-        private int[] v;
-        // n sera la variable que determine hasta donde ira nuestro vector
+        private int [] V;
         private int n;
 
-        //constructor
+        //Crear Constructores
         public Vector(int n) {
             this.n = n;
-            v = new int[this.n + 1];
-            // new int 
+            V = new int[this.n + 1];
+            //aqui  porque estoy sumando un uno porque creo que vamos a empezar nuestro for desde 1
         }
 
         public void LlenarVector() {
-            Console.WriteLine("Introduzca {0} numeros :", n);
+            Console.WriteLine("Introduzca {0} numeros ", n);
             for (int i = 1; i <= n; i++) {
-                Console.Write("\tV[" + i + "]=");
-                v[i] = int.Parse(Console.ReadLine());
+                Console.Write("V["+i+"] = ");
+                V[i] = Int32.Parse(Console.ReadLine());
             }
         }
 
         public void mostrarVector() {
-            //declarar variable i 
-            int i;
-            Console.Write("V[");
-            for (i = 1; i < n; i++) 
-                Console.Write(v[i] + ",");
-            Console.WriteLine(v[n] + "]");
-
-
+            Console.Write("\tV[");
+            for (int i = 1; i <n; i++) 
+                Console.Write(+V[i] + ",");
+            Console.Write(V[n]+" ]");
 
         }
-        
+
+        //hacer la suma de de los elementos del vector
+        public int SumaDeElementos() {
+            int suma = 0;
+            for (int i = 1; i <= n; i++) 
+                suma = suma + V[i];
+            
+            return suma;
+        }
+
+        // metodo de ordenamiento BURBUJA
+
+        public void OrdenarVector() {
+            int i, j;
+            Console.WriteLine("\nOrdenar el Vector");
+            //Metodo de ordenacion Burbuja
+            for (i = 1; i < n; i++) {
+                for (j = i + 1; j <= n; j++) {
+                    //y aqui vamos a preguntar cual es el mayor
+
+                    if (V[i] > V[j]) {
+                        V[i] = V[i] + V[j];
+                        V[j] = V[i] - V[j];
+                        V[i] = V[i] - V[j];
+                    }
+                }
+            }
+        }
+       
+
+
 
 
     }
